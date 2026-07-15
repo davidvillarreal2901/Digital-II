bc45f17152ce6166c20b07904256e311859c2a3d
Davidvillarreal2901 <davidvillarrealar@gmail.com>
2026-07-15 15:05:02 -0500

    feat(integración): agregué una demo automática por UART, todo parece funcionar bien, falta subir la documentación y hacer un makefile con comandos más parchados.

---
+++ firmware/demo_automatica/main.c
@@ -0,0 +1,180 @@
+/*
+ * Demostración automática del sistema integrado.
+ *
+ * Reutiliza el procesamiento de comandos del firmware interactivo
+ * y ejecuta una secuencia continua sin esperar entradas por UART.
+ */
+
+#define main main_interactivo_no_usado
+#include "../demo_integrada/main.c"
+#undef main
+
+
+#define PAUSA_OPERACION_MS  2200u
+#define PAUSA_TEXTO_MS      3200u
+#define PAUSA_CORTA_MS       700u
+
+
+static void copiar_comando(
+    char *destino,
+    const char *origen
+)
+{
+    uint8_t indice;
+
+    indice = 0;
+
+    while (
+        origen[indice] != '\0'
+        && indice < (LINE_SIZE - 1u)
+    ) {
+        destino[indice] = origen[indice];
+        indice++;
+    }
+
+    destino[indice] = '\0';
+}
+
+
+static void ejecutar_comando_demo(
+    const char *comando,
+    uint32_t pausa_ms
+)
+{
+    char linea[LINE_SIZE];
+
+    copiar_comando(
+        linea,
+        comando
+    );
+
+    uart_puts_raw("\r\n[DEMO] ");
+    uart_puts_raw(linea);
+    uart_puts_raw("\r\n");
+
+    /*
+     * Se usa el mismo parser y las mismas funciones
+     * del firmware interactivo.
+     */
+    process_line(linea);
+
+    hub75_ui_delay_ms(pausa_ms);
+}
+
+
+static void inicializar_demo(void)
+{
+    uart_ev_pending_write(0xffffffff);
+    uart_drain_rx();
+
+    uart_puts_raw(
+        "\r\n"
+        "========================================\r\n"
+        " DEMO AUTOMATICA - DIGITAL II\r\n"
+        " CALCULADORA + OLED + WS2812 + HUB75\r\n"
+        "========================================\r\n"
+    );
+
+    oled_init();
+    oled_show_home();
+
+    matrix_clear();
+    hub75_clear();
+
+    hub75_ui_delay_ms(1000u);
+}
+
+
+int main(void)
+{
+    inicializar_demo();
+
+    while (1) {
+
+        /*
+         * Presentación del proyecto.
+         */
+        ejecutar_comando_demo(
+            "T DEMO DIGITAL II",
+            PAUSA_TEXTO_MS
+        );
+
+        /*
+         * OLED: menú de ayuda.
+         * WS2812: imagen.
+         * HUB75: animación.
+         */
+        ejecutar_comando_demo(
+            "H",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * Suma.
+         */
+        ejecutar_comando_demo(
+            "12+5",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * Resta con resultado negativo.
+         */
+        ejecutar_comando_demo(
+            "11-12",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * Multiplicación.
+         */
+        ejecutar_comando_demo(
+            "7*6",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * División.
+         */
+        ejecutar_comando_demo(
+            "100/4",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * Raíces consecutivas.
+         */
+        ejecutar_comando_demo(
+            "R144",
+            PAUSA_OPERACION_MS
+        );
+
+        ejecutar_comando_demo(
+            "R81",
+            PAUSA_OPERACION_MS
+        );
+
+        ejecutar_comando_demo(
+            "R9",
+            PAUSA_OPERACION_MS
+        );
+
+        /*
+         * Texto libre en OLED y HUB75.
+         */
+        ejecutar_comando_demo(
+            "T FPGA LITEX",
+            PAUSA_TEXTO_MS
+        );
+
+        /*
+         * Limpiar antes de repetir.
+         */
+        ejecutar_comando_demo(
+            "C",
+            PAUSA_CORTA_MS
+        );
+    }
+
+    return 0;
+}
---
+++ firmware/demo_automatica/main.d
@@ -0,0 +1,40 @@
+main.o: main.c ../demo_integrada/main.c \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/stdint.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/cdefs.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/config.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/ieeefp.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/features.h \
+ /Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/../libc/picolibc.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_default_types.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_stdint.h \
+ /Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_intsup.h \
+ /Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/generated/csr.h \
+ /Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/generated/soc.h \
+ /Users/david/litex/litex/litex/soc/software/include/system.h \
+ /opt/homebrew/Cellar/riscv-gnu-toolchain/main/lib/gcc/riscv64-unknown-elf/15.1.0/include/stddef.h \
+ /Users/david/litex/litex/litex/soc/cores/cpu/vexriscv/system.h \
+ /Users/david/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
+ /Users/david/litex/litex/litex/soc/software/include/hw/common.h \
+ ../demo_integrada/ws2812_ui.h ../demo_integrada/imagen_ws2812.h \
+ ../demo_integrada/hub75_ui.h ../demo_integrada/hub75_asset.h
+../demo_integrada/main.c:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/stdint.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/cdefs.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/config.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/ieeefp.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/features.h:
+/Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/../libc/picolibc.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_default_types.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_stdint.h:
+/Users/david/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_intsup.h:
+/Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/generated/csr.h:
+/Users/david/Documents/Unal/2026-1/Digital_II/Digital-II--FPGA/build/colorlight_i5/software/include/generated/soc.h:
+/Users/david/litex/litex/litex/soc/software/include/system.h:
+/opt/homebrew/Cellar/riscv-gnu-toolchain/main/lib/gcc/riscv64-unknown-elf/15.1.0/include/stddef.h:
+/Users/david/litex/litex/litex/soc/cores/cpu/vexriscv/system.h:
+/Users/david/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
+/Users/david/litex/litex/litex/soc/software/include/hw/common.h:
+../demo_integrada/ws2812_ui.h:
+../demo_integrada/imagen_ws2812.h:
+../demo_integrada/hub75_ui.h:
+../demo_integrada/hub75_asset.h:
